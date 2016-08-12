d147882d925d35129d0b1c20837c54b69b7ac7b5
thekthuser <thekthuser@thekthuser.com>
2016-08-12 19:54:58 -0400

    created member model

--- teams/models.py
+++ teams/models.py
@@ -8,10 +8,12 @@ class Team(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
 
-"""
 class Member(models.Model):
     user = models.ForeignKey(User, unique=True)
+    username = models.CharField(max_length=255)
     phone = models.CharField(max_length=15)
     email = models.CharField(max_length=255)
+    connoisseur = models.BooleanField()
+    team = models.ForeignKey(Team)
     
-"""
+    USERNAME_FIELD = 'username'
