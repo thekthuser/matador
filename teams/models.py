d67fd4c09c48b7070a2bddd9eb6cd4aba12f1860
thekthuser <thekthuser@thekthuser.com>
2016-08-12 18:59:00 -0400

    created Team model

--- teams/models.py
+++ teams/models.py
@@ -1,5 +1,17 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Team(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+"""
+class Member(models.Model):
+    user = models.ForeignKey(User, unique=True)
+    phone = models.CharField(max_length=15)
+    email = models.CharField(max_length=255)
+    
+"""
