53efb79cf89d3f7ec06e79a9e0b33490d8af4f40
thekthuser <thekthuser@thekthuser.com>
2016-08-18 15:22:48 -0400

    test team forms

--- teams/tests.py
+++ teams/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from teams.models import Team, Member
+from teams.forms import AddTeamForm, RegisterForm, EditMemberForm
 
-# Create your tests here.
 class TeamsModelTestCase(TestCase):
     def setUp(self):
         valor = Team(name='Team Valor', description='Red team in Pokemon Go.')
@@ -14,18 +14,50 @@ class TeamsModelTestCase(TestCase):
 
     def test_team(self):
         valor = Team.objects.get(name='Team Valor')
-        #valor = Team.objects.get(id=1)
-
-        #self.assertEqual(valor.name, 'Team Valor')
         self.assertEqual(valor.description, 'Red team in Pokemon Go.')
 
     def test_member(self):
-        #default = Member.objects.get(id=1)
         default = Member.objects.get(username='defaultMember')
         valor = Team.objects.get(name='Team Valor')
 
-        #self.assertEqual(default.username, 'defaultMember')
         self.assertEqual(default.phone, '555-5555')
         self.assertEqual(default.email, 'default@example.com')
         self.assertEqual(default.connoisseur, True)
         self.assertEqual(default.team, valor)
+
+
+class TeamsFormsTestCase(TestCase):
+    def setUp(self):
+        team_form_data = {'name': 'Team Mystic', 'description': 'Blue team in Pokemon Go.'}
+        teamForm = AddTeamForm(team_form_data)
+        teamForm.save()
+
+        mystic = Team.objects.get(name='Team Mystic')
+        register_form_data = {'username': 'mysticMember', 'phone': '555-5555', 'email': \
+                'mystic@example.com', 'connoisseur': True, 'team': mystic.id, \
+                'password1': 'qwertyqwerty', 'password2': 'qwertyqwerty'}
+        registerForm = RegisterForm(register_form_data)
+        registerForm.save()
+
+    def test_team_form(self):
+        mystic = Team.objects.get(name='Team Mystic')
+        self.assertEqual(mystic.description, 'Blue team in Pokemon Go.')
+
+    def test_register_form(self):
+        default = Member.objects.get(username='mysticMember')
+        self.assertEqual(default.email, 'mystic@example.com')
+        self.assertEqual(default.phone, '555-5555')
+        self.assertTrue(default.connoisseur)
+        self.assertEqual(default.team.name, 'Team Mystic')
+
+    def test_edit_member_form(self):
+        default = Member.objects.get(username='mysticMember')
+        login = self.client.login(username=default.username, password='qwertyqwerty')
+        edited_form_data = {'email': 'new@example.com', 'phone': '555-1234'}
+        editedForm = EditMemberForm(edited_form_data, user=default)
+        if editedForm.is_valid():
+            editedForm.save()
+
+        edited = Member.objects.get(username='mysticMember')
+        self.assertEqual(edited.email, 'new@example.com')
+        self.assertEqual(edited.phone, '555-1234')
