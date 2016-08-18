7d35f06f53d7e4c58e394d4d351be1271629c06b
thekthuser <thekthuser@thekthuser.com>
2016-08-18 15:23:00 -0400

    test restaurants models and forms

--- restaurants/tests.py
+++ restaurants/tests.py
@@ -1,3 +1,39 @@
 from django.test import TestCase
+from restaurants.models import Restaurant, Review
+from restaurants.forms import AddRestaurantForm, AddReviewForm
+from teams.models import Member, Team
 
-# Create your tests here.
+class RestaurantFormsTestCase(TestCase):
+    def setUp(self):
+        res_form_data = {'name': 'Res1', 'description': 'This is Res1.', 'address': \
+            'Grand Central Terminal, New York, NY 10017'}
+        resForm = AddRestaurantForm(res_form_data)
+        resForm.save()
+
+        valor = Team(name='Team Valor', description='Red team in Pokemon Go.')
+        valor.save()
+
+        member = Member(username='defaultMember', password='', phone='555-5555', \
+            email='default@example.com', connoisseur=True, team=valor)
+        member.set_password('qwertyqwerty')
+        member.save()
+
+    def test_add_restaurant_form(self):
+        res = Restaurant.objects.get(name='Res1')
+        self.assertEqual(res.description, 'This is Res1.')
+        self.assertEqual(res.address, 'Grand Central Terminal, New York, NY 10017')
+        self.assertEqual(res.latlon, '40.7527233,-73.977262304')
+
+    def test_add_review_form(self):
+        res = Restaurant.objects.get(name='Res1')
+        member = Member.objects.get(username='defaultMember')
+
+        review_form_data = {'restaurant': res, 'member': member, 'comment': 'Test comment.', \
+            'disliked': False, 'team': member.team}
+        review = AddReviewForm(review_form_data)
+        review.save()
+
+        review = Review.objects.get(restaurant=res)
+        self.assertEqual(review.comment, 'Test comment.')
+
+
